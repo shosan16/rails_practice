5887a5a7dfcfbb56b508c0de4d3ad0d17e39562d
Shota Nagayama <30305829kasukabe@gmail.com>
2021-10-28 08:25:46 +0900

    一覧表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
   get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
 end
+
+#`HTTPメソッド 'ユーザーが指定するURL', to: 'コントローラー名#アクション名'`
