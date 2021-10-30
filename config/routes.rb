a8e64122bc6f362bc9be0325e726031bc5edfe59
Shota Nagayama <30305829kasukabe@gmail.com>
2021-10-31 08:50:52 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
+  get '/users/:id/edit', to: 'users#edit'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 end
 
 #`HTTPメソッド 'ユーザーが指定するURL', to: 'コントローラー名#アクション名'`
